502e4fd03587a2f59533fd82979f47b38a247b7e
j-bd <recodata.out@gmail.com>
2020-01-10 11:57:46 +0100

    Variables renames

--- mnist_gan/gan.py
+++ mnist_gan/gan.py
@@ -27,6 +27,7 @@ class Gan:
         self.generator = self.define_generator()
         self.discriminator = self.define_discriminator()
         self.gan = self.define_gan(self.generator, self.discriminator)
+        self.gan.summary()
 
     def adam_optimizer(self):
         '''Define optimizer type'''
@@ -80,31 +81,31 @@ class Gan:
         '''Define the gan training pipeline'''
         for i in range(self.n_steps):
             # generate points in the latent space
-            z = np.random.randn(self.latent_dim * self.n_batch)
+            latent_space = np.random.randn(self.latent_dim * self.n_batch)
             # reshape into a batch of inputs for the network
-            z = z.reshape(self.n_batch, self.latent_dim)
+            latent_space = latent_space.reshape(self.n_batch, self.latent_dim)
             # generate fake images
-            X_fake = self.generator.predict(z)
+            x_fake = self.generator.predict(latent_space)
             # select a batch of random real images
-            ix = np.random.randint(0, self.size, self.n_batch)
+            real_sample = np.random.randint(0, self.size, self.n_batch)
             # retrieve real images
-            X_real = dataset[ix]
+            x_real = dataset[real_sample]
             # update weights of the discriminator model
             # define target labels for fake images
             y_fake = np.zeros((self.n_batch, 1))
             self.discriminator.trainable = True
             # update the discriminator for fake images
-            self.discriminator.train_on_batch(X_fake, y_fake)
+            self.discriminator.train_on_batch(x_fake, y_fake)
             # define target labels for real images
             y_real = np.ones((self.n_batch, 1))
             # update the discriminator for real images
-            self.discriminator.train_on_batch(X_real, y_real)
+            self.discriminator.train_on_batch(x_real, y_real)
 
             # generate points in the latent space
-            z = np.random.randn(self.latent_dim * self.n_batch)
+            latent_space = np.random.randn(self.latent_dim * self.n_batch)
             # reshape into a batch of inputs for the network
-            z = z.reshape(self.n_batch, self.latent_dim)
+            latent_space = latent_space.reshape(self.n_batch, self.latent_dim)
             # define target labels for real images
             y_real = np.ones((self.n_batch, 1))
             # update generator model
-            self.gan.train_on_batch(z, y_real)
+            self.gan.train_on_batch(latent_space, y_real)
